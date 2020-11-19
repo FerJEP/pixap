e7d013c8bdefeae1b2236b36b9ab72523a27a906
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-11-19 12:16:25 -0400

    better consistency in navbar & saveImage code

--- src/scripts/navbar.ts
+++ src/scripts/navbar.ts
@@ -1,3 +1,5 @@
+import { saveImage } from './saveImage'
+
 const navbarBtn = document.getElementById('navbar-btn')
 const navbarMobile = document.getElementById('navbar-mobile')
 
@@ -6,3 +8,21 @@ if (!navbarBtn || !navbarMobile) throw new Error('Navbar element do not exist!')
 navbarBtn.onclick = () => {
   navbarMobile.classList.toggle('show')
 }
+
+//elements
+
+//Save image
+const saveBtn = document.getElementById('save-btn')
+const fileNameInput = document.getElementById(
+  'filename-input'
+) as HTMLInputElement
+
+saveBtn?.addEventListener('click', () => saveImage(fileNameInput.value))
+
+// Settings menu
+const settingsBtn = document.getElementById('settings-btn')
+const appSettings = document.getElementById('app-settings')
+
+settingsBtn?.addEventListener('click', () => {
+  appSettings?.classList.toggle('show')
+})
--- src/scripts/saveImage.ts
+++ src/scripts/saveImage.ts
@@ -1,16 +1,9 @@
 import { canvas } from '../canvas'
 
-const saveBtn = document.getElementById('save-btn')
-const fileNameInput = document.getElementById('filename-input')
+export function saveImage(filename: string = 'Untiled') {
+  const saveLink = document.createElement('a')
 
-const saveLink = document.createElement('a')
-
-if (!(saveBtn instanceof HTMLButtonElement)) throw new Error('Invale save btn!')
-if (!(fileNameInput instanceof HTMLInputElement))
-  throw new Error('Invale filename input!')
-
-saveBtn?.addEventListener('click', () => {
   saveLink.href = canvas.toDataURL()
-  saveLink.download = fileNameInput.value + '.png'
+  saveLink.download = filename + '.png'
   saveLink.click()
-})
+}
