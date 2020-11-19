6a3b842588f793cd91f1ecc2bd72a0950b095cd1
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-11-19 11:18:26 -0400

    right instance fix

--- src/scripts/saveImage.ts
+++ src/scripts/saveImage.ts
@@ -5,7 +5,7 @@ const fileNameInput = document.getElementById('filename-input')
 
 const saveLink = document.createElement('a')
 
-if (!(saveBtn instanceof HTMLElement)) throw new Error('Invale save btn!')
+if (!(saveBtn instanceof HTMLButtonElement)) throw new Error('Invale save btn!')
 if (!(fileNameInput instanceof HTMLInputElement))
   throw new Error('Invale filename input!')
 
