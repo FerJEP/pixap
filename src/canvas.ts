8ca0d313fd905ac7f5cb80e55363b1fbf2e6cd24
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-11-21 13:52:17 -0400

    +checkered background

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,13 +1,20 @@
 const canvas = document.getElementById('canvas-drawing') as HTMLCanvasElement
+const canvasBackground = document.getElementById(
+  'canvas-background'
+) as HTMLCanvasElement
 
-if (!(canvas instanceof HTMLCanvasElement))
-  throw new Error('Invalid Canvas element')
+if (!canvas || !canvasBackground) throw new Error('Invalid canvas elements')
 
 const cx = canvas.getContext('2d')!
 
+// cx.ImageSmoothingEnabled = false
 ImageSmoothingFalse()
+checkeredCanvas('#d3d3d3', '#ffffff')
 
-canvas.addEventListener('resize', ImageSmoothingFalse)
+canvas.addEventListener('resize', () => {
+  ImageSmoothingFalse()
+  checkeredCanvas('#d3d3d3', '#ffffff')
+})
 
 export { canvas, cx }
 
@@ -22,4 +29,18 @@ function ImageSmoothingFalse() {
   cx.msImageSmoothingEnabled = false
   //@ts-ignore
   cx.oImageSmoothingEnabled = false
+}
+
+function checkeredCanvas(color1: string, color2: string) {
+  canvasBackground.width = canvas.width
+  canvasBackground.height = canvas.height
+
+  const cx = canvasBackground.getContext('2d')!
+
+  for (let x = 0; x < canvasBackground.width; x++) {
+    for (let y = 0; y < canvasBackground.height; y++) {
+      cx.fillStyle = (x + y) % 2 === 0 ? color1 : color2
+      cx.fillRect(x, y, 1, 1)
+    }
+  }
 }
