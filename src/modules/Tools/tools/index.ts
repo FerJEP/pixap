ae5ec5a59cc25480acc1056b648c3b39a0d9b70d
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-11-23 20:55:55 -0400

    +square tool

--- src/modules/Tools/tools/index.ts
+++ src/modules/Tools/tools/index.ts
@@ -1,8 +1,9 @@
 import { pencil } from './pencil'
 import { eraser } from './eraser'
 import { colorPicker } from './colorSelector'
+import { square } from './square'
 
 // All the tools must be imported here, and added to the exported
 // default array to be added to the tools container by ToolsHandler.ts
 
-export const AllTools = [pencil, eraser, colorPicker]
+export const AllTools = [pencil, eraser, square, colorPicker]
---
+++ src/modules/Tools/tools/square.ts
@@ -0,0 +1,44 @@
+import { lineAlgo } from '../../../scripts/lineAlgo'
+import { Tool, ToolMethod } from './Tool'
+
+const squareElement = Tool.createElement('button', 'bx-bx-rectangle')
+const squareMethod: ToolMethod = (cx, mouse) => {
+  if (mouse.move && mouse.down) {
+    const width = mouse.move.currentX - mouse.down.x
+    const height = mouse.move.currentY - mouse.down.y
+
+    cx.clearRect(0, 0, cx.canvas.width, cx.canvas.height)
+
+    if (width === 0) {
+      // if It's a vertical line
+      lineAlgo(
+        mouse.down.x,
+        mouse.down.y,
+        mouse.down.x,
+        mouse.move.currentY,
+        (x, y) => {
+          cx.fillRect(x, y, 1, 1)
+        }
+      )
+    } else if (height === 0) {
+      // if It's a horizontal line
+      lineAlgo(
+        mouse.down.x,
+        mouse.down.y,
+        mouse.move.currentX,
+        mouse.down.y,
+        (x, y) => {
+          cx.fillRect(x, y, 1, 1)
+        }
+      )
+    } else {
+      // else, it's finally a square
+      cx.strokeRect(mouse.down.x + 0.5, mouse.down.y + 0.5, width, height)
+    }
+  } else if (mouse.down) {
+    //if mouse hasn't moved yet
+    cx.clearRect(0, 0, cx.canvas.width, cx.canvas.height)
+    cx.fillRect(mouse.down.x, mouse.down.y, 1, 1)
+  }
+}
+export const square = new Tool('square', squareElement, squareMethod)
