6baa61cb8bdadb943da7095eed295537b53f6270
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-12-10 06:33:21 -0400

    opening settings when app starts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import './styles/index.style' //Main style file
-import './scripts/saveImage' //Save image handler
 
 //Modules
-import './modules/Settings'
+import { openSettings } from './modules/Settings'
 import './modules/Tools/index'
 import './modules/Controllers'
 import './modules/Navbar' //Navbar mobile handler
+
+openSettings()
