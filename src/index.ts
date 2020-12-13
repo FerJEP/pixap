8171839106418f7235c7600a463745ce90a665e8
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-12-13 15:14:08 -0400

    importing canvas.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './styles/index.style' //Main style file
 
+import './canvas'
+
 //Modules
 import { openSettings } from './modules/Settings'
 import './modules/Navbar' //Navbar mobile handler
