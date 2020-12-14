913f247470ccbbdca7a696da5a128759125bfd85
Fernando Esparragoza <fernando.fjep@gmail.com>
2020-12-14 19:53:24 -0400

    fit to screen when app starts

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,7 @@ import './modules/CanvasPointerEvents'
 import './modules/Tools/index'
 import './modules/Controllers'
 
+// fit to screen
+import { fitToScreen } from './modules/Controllers/controllers/fitToScreen'
+fitToScreen.action()
 openSettings()
